199708352244765da31aa39368986b25259d2539
Your NameRyan <Rymaster95@gmail.com>
2018-12-09 14:36:26 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from './chat.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'front-end';
+  message: string;
+  messages: string[] = [];
+
+  constructor(public chatService: ChatService){
+
+  }
+  sendMessage(){
+    this.chatService.sendMessage(this.message);
+    this.message = '';
+  }
+
+  ngOnInit(): void{
+    this.chatService
+    .getMessages()
+    .subscribe((message: string) => {
+      this.messages.push(message);
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatService } from './chat.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
-  providers: [],
+  providers: [ChatService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
